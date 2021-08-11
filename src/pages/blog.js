6ea4ce02465d87de8ae4ffc993e8aad608760745
Luke Hinrichs <luke.hinrichs@gmail.com>
2021-08-10 22:37:13 -0600

    finished importing blog content and installing MDX

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,32 +1,45 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const blogPage = ({data}) => {
     return (
-        <Layout pageTitle="My Blog Post">
-            <ul>
-                {
-                    data.allFile.nodes.map(node =>(
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+        <Layout pageTitle="My Blog Post">{
+            data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <h6>{node.frontmatter.author}</h6>
+                        <p>{node.frontmatter.date}</p>
+                        <hr />
+                        <MDXRenderer>
+                            {node.body}
+                        </MDXRenderer>
+                        <hr />
+                        <p>{node.timeToRead} minute read</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
-query {
-    allFile {
+query MyQuery {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          author
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
         id
+        body
+        timeToRead
       }
     }
   }
+  
   
 `
 
