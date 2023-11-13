ade102732b9ed979ac1dab5f11ad776b3974993c
Luke Hinrichs <luke.hinrichs@gmail.com>
2023-11-13 10:18:44 -0700

    added linking blog pages (section 6)

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 //this takes the "data" varaible created from the export below unpacks it then we map to get the value
 const BlogPage = ({ data }) => {
@@ -9,9 +9,12 @@ const BlogPage = ({ data }) => {
         <Layout pageTitle="My Blog Posts"> {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.slug}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
           </article>
         ))
       }
@@ -27,6 +30,7 @@ query {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt
