433a57fb487ae384ac765367ed6f10e502533dd7
Luke Hinrichs <luke.hinrichs@gmail.com>
2023-11-13 10:18:44 -0700

    Revert "added linking blog pages (section 6)"
    This reverts commit ade102732b9ed979ac1dab5f11ad776b3974993c.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { Link, graphql } from 'gatsby'
-import Layout from '../../components/layout'
-import Seo from '../../components/seo'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 //this takes the "data" varaible created from the export below unpacks it then we map to get the value
 const BlogPage = ({ data }) => {
@@ -9,12 +9,9 @@ const BlogPage = ({ data }) => {
         <Layout pageTitle="My Blog Posts"> {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>
-              <Link to={`/blog/${node.frontmatter.slug}`}>
-                {node.frontmatter.slug}
-              </Link>
-            </h2>
+            <h2>{node.frontmatter.title}</h2>
             <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
           </article>
         ))
       }
@@ -30,7 +27,6 @@ query {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
-          slug
         }
         id
         excerpt
