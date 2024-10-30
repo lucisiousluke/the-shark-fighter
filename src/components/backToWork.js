42832a6cc5fcada6da8cbf8fd5884bc6fa7efbb6
Luke Hinrichs <luke.hinrichs@gmail.com>
2024-10-30 17:43:00 -0600

    additional styling

--- src/components/backToWork.js
+++ src/components/backToWork.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 const BackToWork = () => {
     return (
-        <div className="my-6 text-center">
+        <div className="my-10 text-center">
             <Link className="bg-transparent hover:bg-gray-100 text-cyan-500 font-semibold py-4 px-8 border border-cyan-500 rounded-lg shadow" to='/portfolio'>Back to Work</Link>
         </div>
     )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,24 +5,26 @@ const ContactPage = () => {
     return (
         <main>
             <Layout pageTitle="Contact Me">
-                <section className="grid grid-cols-4 gap-4">
-                    <div className="bg-cyan-300 rounded-lg col-span-3">
-                        <h1 className="text-3xl font-bold">Contact Me</h1>
+                <section className="grid grid-cols-4 gap-4 max-w-7xl mx-auto">
+                    <div className="bg-cyan-900 text-white rounded-lg col-span-3 p-6">
+                        <h1 className="text-3xl font-thin mb-2">Contact Me</h1>
                         <p>UX/UI research, web developent, brand management</p>
-                        <div className="grid grid-cols-2 gap-4">
-                            <div className="font-bold">
-                                Phone number
+                        <div className="grid grid-cols-2 gap-4 mt-5">
+                            <div>
+                                <p className="font-bold">Phone number</p>
+                                <a className="hover:underline" href="tel:3033969904">303-396-9904</a>
                             </div>
-                            <div className="font-bold">
-                                Email
+                            <div>
+                                <p className="font-bold">Email</p>
+                                <a className="hover:underline" href="mailto:luke.hinrichs@gmail.com">luke.hinrichs@gmail.com</a>
                             </div>
-                            <div className="font-bold">
-                                Location
+                            <div>
+                                <p className="font-bold">Location</p>
+                                <p>Denver, Co</p>
                             </div>
                         </div>
                     </div>
                     <div className="bg-rose-500	rounded-lg">
-
                     </div>
                 </section>
             </Layout>
