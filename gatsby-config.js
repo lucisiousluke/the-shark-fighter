1a7091e4d4a0c925e96b3020f3314eaae94a3100
Luke Hinrichs <luke.hinrichs@gmail.com>
2024-11-05 09:26:03 -0700

    image file structure fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,14 @@
       options: {
         name: `portfolio`,
         path: `${__dirname}/portfolio`,
-      }
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
