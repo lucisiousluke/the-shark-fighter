d3ec39d4393564e35ed6c5df2f86f3ecb74ff01d
Luke Hinrichs <luke.hinrichs@gmail.com>
2024-11-12 09:52:10 -0700

    Footer Contact info

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,46 @@
-import * as React from 'react'
-import PrimaryNavigation from './navigation'
+import { Link } from "gatsby";
+import * as React from "react";
+import PrimaryNavigation from "./navigation";
 
 const SiteFooter = () => {
-    return (
-        <footer className="bg-slate-300 py-6 mt-5">
-            <section className="container mx-auto ">
-                <p>small excerpt about my experence</p>
-                <p>Copywrite 2023 The Shark Fighter</p>
-            </section>
-            <div>
-            <PrimaryNavigation />
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="mt-20">
+      <div class="mx-auto border-t border-gray-300 text-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 border-gray-300 md:border-l divide-gray-300 leading-7 font-extralight">
+          <div className="py-10 border-b md:border-b-0 md:border-r">
+            <h2 className="text-2xl mb-4 text-cyan-500">Contact</h2>
+            <p>Luke Hinrichs</p>
+            <p>
+              <Link
+                href="mailto:luke.hinrichs@gmail.com"
+                className="text-blue-500 hover:underline"
+              >
+                luke.hinrichs@gmail.com
+              </Link>
+            </p>
+            <p>303.396.9904</p>
+            <p>Denver, CO</p>
+          </div>
+          <div className="py-10">
+            <h2 className="text-2xl mb-4 text-cyan-500">Follow Me</h2>
+            <ul className="space-y-2">
+              <li>
+                <a href="#" className="text-blue-500 hover:underline">
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
 
-export default SiteFooter
+      <section className="mx-auto text-white font-extralight bg-cyan-500 py-3 ">
+        <div className="container">
+          <p>Copywrite 2023 The Shark Fighter</p>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default SiteFooter;
