3d1b729215635134dbc3b608a0f67abf5d3fc7af
Luke Hinrichs <luke.hinrichs@bio-techne.com>
2025-04-23 11:22:16 -0600

    styles added inline

--- src/components/backgroundImage.js
+++ src/components/backgroundImage.js
@@ -16,7 +16,15 @@ const BackgroundSection = ({ imageData, overlayImageData, children }) => {
         <GatsbyImage
           image={overlayImageData}
           alt="Overlay"
-          className="absolute -top-20 right-36 md:right-48 w-[425px] h-auto object-contain z-10"
+          style={{
+            position: 'absolute',
+            top: '-80px',
+            right: '150px',
+            width: '425px',
+            zIndex: 10,
+            objectFit: 'contain',
+          }}
+          className="rounded-xl"
         />
       )}
 
