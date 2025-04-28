9e01c0a5f17706643299dc01f73815b102336f5b
Luke Hinrichs <luke.hinrichs@bio-techne.com>
2025-04-28 09:12:43 -0600

    responsive portfolio

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -10,20 +10,20 @@ const PortfolioPost = ({ data }) => {
 
   return (
     <Layout>
-      <PageTitle 
-        pageTitle="My work" 
+      <PageTitle
+        pageTitle="My work"
         pageDescription="Not to bad if I do say so myself"
       />
-      <div className="grid grid-cols-3 gap-6 max-w-7xl mx-auto my-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-7xl mx-auto my-20">
         {data.allMdx.nodes.map((node) => {
           const portfolioThumbnail = getImage(node.frontmatter.hero_image) // Access hero_image here
           const excerpt = (node.excerpt)
           return (
             <article key={node.id}>
               <div className='rounded'>
-              {portfolioThumbnail && (
-                <GatsbyImage image={portfolioThumbnail} alt={node.frontmatter.title} className="rounded-lg h-64 w-full object-cover"/>
-              )}
+                {portfolioThumbnail && (
+                  <GatsbyImage image={portfolioThumbnail} alt={node.frontmatter.title} className="rounded-lg h-64 w-full object-cover" />
+                )}
               </div>
               <div className="mx-5 mt-4 mb-6">
                 <h2 className="font-thin text-3xl mb-1 text-cyan-500">
