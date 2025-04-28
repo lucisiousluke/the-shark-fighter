16b9b7062249977ef67256f1fabd7554e297755b
Luke Hinrichs <luke.hinrichs@bio-techne.com>
2025-04-28 09:36:31 -0600

    contact us mobile styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import PageTitle from '../components/pageTitle'
 
-const ContactPage = ({data}) => {
+const ContactPage = ({ data }) => {
     const image = getImage(data.yourImage);
 
     return (
@@ -14,11 +14,11 @@ const ContactPage = ({data}) => {
                     pageTitle="Contact Me"
                     pageDescription="Think I can help? Reach out."
                 />
-                <section className="grid grid-cols-4 gap-4 max-w-7xl mx-auto my-20 h-96">
+                <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 max-w-7xl mx-auto my-20 h-96">
                     <div className="bg-slate-50 text-white rounded-lg col-span-3 p-6">
                         <h1 className="text-3xl font-thin mb-2 text-cyan-500">Contact Me</h1>
-                        <p className="text-slate-500">UX/UI research, web developent, brand management</p>
-                        <div className="grid grid-cols-2 gap-4 mt-5">
+                        <p className="text-slate-500">UX/UI research, web development, brand management</p>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5">
                             <div>
                                 <h2 className="font-bold text-cyan-500">Phone number</h2>
                                 <a className="hover:underline text-slate-500" href="tel:3033969904">303-396-9904</a>
@@ -32,15 +32,17 @@ const ContactPage = ({data}) => {
                                 <p className="text-slate-500">Denver, Co</p>
                             </div>
                         </div>
+
                     </div>
-                    <div className="rounded-lg overflow-hidden">
-                        <GatsbyImage 
-                        image={image} 
-                        alt="Black and white wrestler"
-                        className="w-full h-full object-cover object-left"
+                    <div className="rounded-lg overflow-hidden hidden md:block">
+                        <GatsbyImage
+                            image={image}
+                            alt="Black and white wrestler"
+                            className="w-full h-full object-cover object-left"
                         />
                     </div>
                 </section>
+
             </Layout>
         </main>
     )
