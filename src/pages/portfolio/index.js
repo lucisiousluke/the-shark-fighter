6c6fbfe23b7c238098a1423465b4face162884f9
Luke Hinrichs <luke.hinrichs@bio-techne.com>
2025-11-10 08:49:10 -0700

    fixed too page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -4,69 +4,42 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 import PageTitle from "../../components/pageTitle";
-import Lightbox from "yet-another-react-lightbox";
-import "yet-another-react-lightbox/styles.css";
 
-const PortfolioPost = ({ data }) => {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const [photoIndex, setPhotoIndex] = React.useState(0);
-
-  // Collect all hero_image URLs for lightbox
-  const images = data.allMdx.nodes.map(
-    (node) => ({ src: node.frontmatter.hero_image.publicURL })
-  );
-
+const PortfolioIndex = ({ data }) => {
   return (
     <Layout>
       <PageTitle
         pageTitle="My work"
         pageDescription="Not too bad if I do say so myself"
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-7xl mx-auto my-20">
-        {data.allMdx.nodes.map((node, index) => {
-          const portfolioThumbnail = getImage(node.frontmatter.hero_image);
-          const excerpt = node.excerpt;
 
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-7xl mx-auto my-20">
+        {data.allMdx.nodes.map((node) => {
+          const thumbnail = getImage(node.frontmatter.hero_image);
           return (
             <article key={node.id}>
-              <div
-                className="rounded cursor-pointer"
-                onClick={() => {
-                  setPhotoIndex(index);
-                  setIsOpen(true);
-                }}
-              >
-                {portfolioThumbnail && (
+              {/* Thumbnail links to portfolio page */}
+              <Link to={`/portfolio/${node.frontmatter.slug}`}>
+                {thumbnail && (
                   <GatsbyImage
-                    image={portfolioThumbnail}
+                    image={thumbnail}
                     alt={node.frontmatter.title}
-                    className="rounded-lg h-64 w-full object-cover"
+                    className="rounded-lg w-full h-48 object-cover"
                   />
                 )}
-              </div>
+              </Link>
+
               <div className="mx-5 mt-4 mb-6">
                 <h2 className="font-thin text-3xl mb-1 text-cyan-500">
-                  <Link
-                    to={`/portfolio/${node.frontmatter.slug}`}
-                    state={{ galleryImages: node.frontmatter.gallery_images }}
-                  >
+                  <Link to={`/portfolio/${node.frontmatter.slug}`}>
                     {node.frontmatter.title}
                   </Link>
                 </h2>
-                <p className="text-slate-500">{excerpt}</p>
+                <p className="text-slate-500">{node.excerpt}</p>
               </div>
             </article>
           );
         })}
-
-        {isOpen && (
-          <Lightbox
-            open={isOpen}
-            index={photoIndex}
-            slides={images}
-            onClose={() => setIsOpen(false)}
-          />
-        )}
       </div>
     </Layout>
   );
@@ -83,7 +56,7 @@ export const query = graphql`
           hero_image {
             publicURL
             childImageSharp {
-              gatsbyImageData
+              gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
             }
           }
         }
@@ -94,6 +67,6 @@ export const query = graphql`
   }
 `;
 
-export const Head = () => <Seo title="My Blog Posts" />;
+export const Head = () => <Seo title="My Portfolio" />;
 
-export default PortfolioPost;
+export default PortfolioIndex;
